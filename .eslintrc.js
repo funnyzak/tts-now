5295b53576a0a034772a76194505081f301c430d
Potato <silenceace@gmail.com>
2021-10-28 22:48:37 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,6 +88,7 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/require-default-props': 'off',
     '@typescript-eslint/no-use-before-define': ['warn'],
     'react/jsx-filename-extension': [
       'warn',
