c68b28ad1d8f80664beca8e3f10dceb56d0c3a81
Leon <silenceace@gmail.com>
2021-10-29 13:44:24 +0800

    chore: 场景选择

--- src/layout/SiderBar/Avatar.tsx
+++ src/layout/SiderBar/Avatar.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { useState } from 'react';
-import { Menu, Dropdown } from 'antd';
+import { Menu, Dropdown, Typography } from 'antd';
 import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 import useAppSetting from '@/hook/appHook';
 import { voiceTypeList } from '@/config';
+
+const { Title } = Typography;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -18,36 +20,53 @@ const Wrapper = styled.div`
 const VoiceSelectComponent = (props: any) => {
   const { voiceIndex, voiceSetCallBack } = props;
   return (
-    <Menu
-      onClick={voiceSetCallBack}
+    <div
       css={{
         maxHeight: 'calc(100vh - 280px)',
         overflowY: 'auto',
         overflowX: 'hidden',
         borderSize: 'border-box',
         borderRadius: '5px',
-        border: '1px solid #eee'
+        border: '1px solid #eee',
+        background: '#fff'
       }}
-      selectedKeys={[voiceIndex.toString()]}
     >
-      {voiceTypeList.map((voiceType, index) => (
-        <Menu.Item
-          key={index.toString()}
-          css={{
-            borderBottom: '1px solid #eee',
-            height: '45px !important',
-            margin: '0 auto !important'
-          }}
-          icon={
-            <img src={voiceType.img} css={{ height: '30px', width: 'auto' }} />
-          }
-        >
-          {voiceType.speaker}
-          ，
-          {voiceType.speechType}
-        </Menu.Item>
-      ))}
-    </Menu>
+      <Title
+        css={css`
+          padding: 10px 0 10px 10px;
+          border-bottom: 1px solid #eee;
+        `}
+        level={4}
+      >
+        场景选择
+      </Title>
+      <Menu
+        css={{ border: '0' }}
+        onClick={voiceSetCallBack}
+        selectedKeys={[voiceIndex.toString()]}
+      >
+        {voiceTypeList.map((voiceType, index) => (
+          <Menu.Item
+            key={index.toString()}
+            css={{
+              borderBottom: '1px solid #eee',
+              height: '45px !important',
+              margin: '0 auto !important'
+            }}
+            icon={(
+              <img
+                src={voiceType.img}
+                css={{ height: '30px', width: 'auto' }}
+              />
+            )}
+          >
+            {voiceType.speaker}
+            ，
+            {voiceType.speechType}
+          </Menu.Item>
+        ))}
+      </Menu>
+    </div>
   );
 };
 
