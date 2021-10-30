d4f3399659d8aa152e0874023dcb7721d105112a
Potato <silenceace@gmail.com>
2021-10-30 08:21:43 +0800

    chore: 批量转换

--- src/layout/AppMain/Batch.tsx
+++ src/layout/AppMain/Batch.tsx
@@ -1,10 +1,16 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import { Button } from 'antd';
-import React, { useState, useEffect } from 'react';
-import { RedoOutlined, DeleteOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import {
+  RedoOutlined,
+  DeleteOutlined,
+  PlayCircleOutlined,
+  FolderOpenFilled
+} from '@ant-design/icons';
+import {
+  Button, Table, Tag, Space
+} from 'antd';
 import useAppSetting from '@/hook/appHook';
-import { voiceTypeList } from '@/config';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -20,11 +26,21 @@ const MainWrapper = styled.div`
   display: flex;
 `;
 
+enum FileConvertStatus {
+  READY = '待转换',
+  PROCESS = '合成中',
+  FAIL = '失败',
+  SUCCESS = '成功'
+}
+
 interface FileInfoProp {
+  key: React.Key;
   filePath: string;
   fileName: string;
   textContent: string;
-  complete: boolean;
+  status: FileConvertStatus;
+  wordCount: number;
+  elapsed?: number;
   audioUrls?: Array<string>;
   audioPaths?: Array<string>;
 }
@@ -33,46 +49,111 @@ interface FileListProp {
   fileList?: Array<FileInfoProp>;
 }
 
-const SelectFilesComponent: React.FC<FileListProp> = ({ fileList }) => {
-  if (fileList !== undefined && fileList !== null && fileList.length > 0) {
-    return <MainWrapper>这是列表</MainWrapper>;
-  }
-  return (
-    <MainWrapper
-      css={css`
-        align-items: center;
-        justify-content: center;
-        background-color: #f4f6fa;
-      `}
+const SelectFilesComponent: React.FC<{}> = () => (
+  <MainWrapper
+    css={css`
+      align-items: center;
+      justify-content: center;
+      background-color: #f4f6fa;
+    `}
+  >
+    <Button
+      type="primary"
+      css={{ width: '145px;', borderRadius: '5px' }}
+      size="large"
     >
-      <Button
-        type="primary"
-        css={{ width: '145px;', borderRadius: '5px' }}
-        size="large"
-      >
-        选择文件夹
-      </Button>
+      选择文件夹
+    </Button>
+  </MainWrapper>
+);
+
+const ConvertFilesComponent: React.FC<FileListProp> = ({ fileList }) => {
+  const actionHandler = (action: string, data: FileInfoProp) => {
+    console.log(action, data);
+    if (action === 'play') {
+      console.log(action);
+    } else if (action === 'open') {
+      console.log(action);
+    }
+  };
+
+  return (
+    <MainWrapper>
+      {fileList !== undefined}
+      <Table dataSource={fileList} css={{ width: '100%' }}>
+        <Table.Column title="文件名" dataIndex="fileName" key="fileName" />
+        <Table.Column title="字符数" dataIndex="wordCount" key="wordCount" />
+        <Table.Column title="合成耗时" dataIndex="elapsed" key="elapsed" />
+        <Table.Column
+          title="状态"
+          dataIndex="status"
+          key="status"
+          render={(status: FileConvertStatus) => (
+            <>
+              <Tag color="success">{status.toString()}</Tag>
+            </>
+          )}
+        />
+        <Table.Column
+          title="操作"
+          key="action"
+          render={(_txt, data: FileInfoProp) => (
+            <Space size="middle">
+              <PlayCircleOutlined
+                onClick={() => actionHandler('play', data)}
+                alt="播放音频"
+                style={{
+                  color:
+                    data.status === FileConvertStatus.SUCCESS
+                      ? '#52c41a'
+                      : '#ccc'
+                }}
+              />
+              <FolderOpenFilled
+                onClick={() => actionHandler('open', data)}
+                alt="打开原文件"
+                style={{ color: '#414e62' }}
+              />
+            </Space>
+          )}
+        />
+      </Table>
     </MainWrapper>
   );
 };
 
+const MangageFilesComponent: React.FC<FileListProp> = ({ fileList }) => {
+  if (fileList !== undefined && fileList !== null && fileList.length > 0) {
+    return <ConvertFilesComponent fileList={fileList} />;
+  }
+  return <SelectFilesComponent />;
+};
+
+MangageFilesComponent.defaultProps = {
+  fileList: [
+    {
+      key: '1',
+      filePath: '/path/大同定.txt',
+      fileName: '大同定.txt',
+      textContent: '你好',
+      wordCount: 2,
+      elapsed: 0,
+      status: FileConvertStatus.READY
+    },
+    {
+      key: '2',
+      filePath: '/path/大同定.txt',
+      fileName: '大同定e.txt',
+      textContent: '你好e',
+      wordCount: 3,
+      elapsed: 0,
+      status: FileConvertStatus.READY
+    }
+  ]
+};
 const Index = () => {
   const { appSetting, setAppSetting } = useAppSetting();
   const [fileList, setFileList] = useState<Array<FileInfoProp>>();
-  const [singleTxt, setSingleTxt] = useState(
-    !appSetting.singleTxt
-      ? voiceTypeList[appSetting.voiceSetIndex].text
-      : appSetting.singleTxt
-  );
-
-  useEffect(() => {
-    if (
-      singleTxt
-      && singleTxt !== voiceTypeList[appSetting.voiceSetIndex].text
-    ) {
-      setAppSetting({ singleTxt });
-    }
-  }, [singleTxt]);
 
   return (
     <Wrapper>
@@ -108,7 +189,7 @@ const Index = () => {
           </Button>
         </div>
       </div>
-      <SelectFilesComponent fileList={fileList} />
+      <MangageFilesComponent fileList={fileList} />
     </Wrapper>
   );
 };
