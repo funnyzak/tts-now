1e2643746628bd1105c599bbdc5130d8024e7fe9
Potato <silenceace@gmail.com>
2021-11-04 22:19:35 +0800

    chore: add quit button for windows

--- src/background.ts
+++ src/background.ts
@@ -125,7 +125,19 @@ const menuTemplate: any = [
         ]
       }
     ]
-    : []),
+    : [
+      {
+        label: '程序',
+        submenu: [
+          {
+            label: '退出',
+            click: () => {
+              app.quit();
+            }
+          }
+        ]
+      }
+    ]),
   {
     label: '编辑',
     submenu: [
