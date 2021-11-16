f4f165769127f9bab19c6b299aa64232da026ac2
Potato <silenceace@gmail.com>
2021-11-16 11:16:07 +0800

    chore: appsetting hook

---
+++ src/hook/app.ts
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { AppContext } from '@/config';
+import voiceData from '@/config/voice';
+import { TtsEngine } from '@/type/enums';
+
+const SettingContext = () => useContext(AppContext);
+
+const getVoiceTypeList = (_engine?: TtsEngine) => voiceData[
+  _engine
+    ? _engine.toString()
+    : SettingContext().appSetting.ttsSetting.engine.toString()
+];
+
+const currentSpeaker = (_speakerId?: string, _engine?: TtsEngine) => {
+  const speakerId = _speakerId || SettingContext().appSetting.ttsSetting.speakerId;
+
+  const voiceTypeList = getVoiceTypeList(_engine);
+
+  if (speakerId && speakerId.length === 0) {
+    return voiceTypeList[0];
+  }
+  const _findSpeakers = voiceTypeList.filter(
+    (v) => v.speakerId === speakerId
+  );
+  return _findSpeakers.length > 0 ? _findSpeakers[0] : voiceTypeList[0];
+};
+
+export { getVoiceTypeList, currentSpeaker };
+
+export default SettingContext;
--- src/hook/appHook.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { AppContext } from '@/config';
-
-export default () => useContext(AppContext);
