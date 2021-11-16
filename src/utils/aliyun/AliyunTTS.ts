19d417cb5a30db22f0493de00301feecba349f91
Potato <silenceace@gmail.com>
2021-11-16 21:41:21 +0800

    chore: aliyun tts

--- src/utils/aliyun/AliyunTTS.ts
+++ src/utils/aliyun/AliyunTTS.ts
@@ -8,14 +8,42 @@ const Request = require('request-promise');
 export interface AliTtsOption {
   // https://help.aliyun.com/document_detail/130555.html
   appKey?: string;
+  /**
+   * 音频编码格式，支持pcm/wav/mp3格式，默认是pcm。
+   */
   format?: string;
+  /**
+   * 音频采样率，支持16000Hz和8000Hz，默认是16000Hz。
+   */
   sample_rate?: number;
+  /**
+   * 发音人，默认是xiaoyun。更多发音人请参见接口说明。 https://help.aliyun.com/document_detail/130509.htm?spm=a2c4g.11186623.0.0.442a38adeflvK0#topic-2606811
+   */
   voice?: string;
+  /**
+   * 音量，范围是0~100，默认50。
+   */
   volume?: number;
+  /**
+   * 语速，范围是-500~500，默认是0。
+   */
   speech_rate?: number;
+  /**
+   * 语调，范围是-500~500，默认是0。
+   */
   pitch_rate?: number;
+  /**
+   * 是否启用句级时间戳功能，默认值为false。
+   */
   enable_subtitle?: boolean;
+  /**
+   * 是否启用回调功能，默认值为false。
+   */
   enable_notify?: boolean;
+  /**
+   * 回调服务的地址。当enable_notify取值为true时，本字段必填。
+   * URL支持HTTP/HTTPS协议，Host不能使用IP地址。
+   */
   notify_url?: string;
 }
 
@@ -23,9 +51,21 @@ export interface AliTtsOption {
  * 合成的返回数据定义
  */
 export interface AliTtsComplete {
+  /**
+   * 返回的任务ID
+   */
   task_id: string;
+  /**
+   * 合成的音频URL
+   */
   audio_address: string;
+  /**
+   * 回调地址
+   */
   notify_custom: string;
+  /**
+   * 句级时间戳对象
+   */
   sentences: any;
 }
 
