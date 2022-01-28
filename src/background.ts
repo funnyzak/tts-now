5440332c0194c3426f394b20bbebb8735cd26c73
leon <silenceace@gmail.com>
2022-01-28 18:45:54 +0800

    feat: background

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,26 @@
 import {
   app, BrowserWindow, ipcMain, Menu, dialog, shell
-} from 'electron';
+} from 'electron'
 
-const Store = require('electron-store');
+const Store = require('electron-store')
 
-const config = require('../app.config');
+const config = require('../app.config')
 
-let win: BrowserWindow;
+let win: BrowserWindow
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-const isMac = process.platform === 'darwin';
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isMac = process.platform === 'darwin'
 
-Store.initRenderer();
+Store.initRenderer()
 
 /**
  * 禁止刷新和调试
  */
 function stopKey(_win: BrowserWindow) {
-  const KEY_BLACK_LIST = ['I', 'R'];
-  const FKEY_BLACK_LIST = ['F5', 'F12'];
+  const KEY_BLACK_LIST = ['I', 'R']
+  const FKEY_BLACK_LIST = ['F5', 'F12']
   _win.webContents.on('before-input-event', (event, input) => {
-    console.log('before-input-event', input);
+    console.log('before-input-event', input)
     // input.control 为windows CTRL；input.meta 为mac Ctrl键
     // 以下条件为禁止组合键和F键 刷新和调试
     if (
@@ -28,9 +28,9 @@ function stopKey(_win: BrowserWindow) {
         && KEY_BLACK_LIST.includes(input.key.toUpperCase()))
       || FKEY_BLACK_LIST.includes(input.key.toUpperCase())
     ) {
-      event.preventDefault();
+      event.preventDefault()
     }
-  });
+  })
 }
 
 function createWindow() {
@@ -49,24 +49,24 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false
     }
-  });
+  })
 
   // 禁止页面不可见时停止计时器。防止setTimeout问题
-  win.webContents.setBackgroundThrottling(false);
+  win.webContents.setBackgroundThrottling(false)
 
   if (isDevelopment) {
-    win.loadURL(`http://${config.devServer.host}:${config.devServer.port}`);
-    win.resizable = true;
-    win.webContents.openDevTools();
+    win.loadURL(`http://${config.devServer.host}:${config.devServer.port}`)
+    win.resizable = true
+    win.webContents.openDevTools()
   } else {
     // Load the index.html when not in development
-    win.loadURL(`file://${__dirname}/index.html`);
+    win.loadURL(`file://${__dirname}/index.html`)
 
-    stopKey(win);
+    stopKey(win)
 
     win.on('ready-to-show', () => {
-      win.show();
-    });
+      win.show()
+    })
   }
 }
 
@@ -74,43 +74,87 @@ function createWindow() {
 // 和创建浏览器窗口的时候调用
 // 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', () => {
-  createWindow();
-});
+  createWindow()
+
+  // 选择要转换的文件或其他选择
+  ipcMain.on('select_files', (event, arg) => {
+    console.log(event, arg)
+
+    // https://www.electronjs.org/docs/latest/api/dialog
+    dialog
+      .showOpenDialog(win, {
+        title: '选择文件',
+        buttonLabel: '确定',
+        properties: ['openFile', 'multiSelections'],
+        filters: [{ name: '文件', extensions: ['txt', 'text'] }],
+        ...(arg.config || {})
+      })
+      .then((_rlt) => {
+        console.log(_rlt)
+        event.reply('selected_files', {
+          data: _rlt,
+          action: arg.action
+        })
+      })
+  })
+
+  ipcMain.on('open-external', (_event, arg) => {
+    shell.openExternal(arg)
+  })
+
+  ipcMain.on('open-close-dialog', (_event, _arg) => {
+    dialog
+      .showMessageBox(win, {
+        type: 'info',
+        title: '提示',
+        message: '是否关闭应用程序？',
+        buttons: ['取消', '关闭']
+      })
+      .then((res) => {
+        if (res.response === 1) {
+          app.exit()
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  })
+})
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
 
 // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+  if (process.platform !== 'darwin') app.quit()
+})
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
       if (data === 'graceful-exit') {
-        app.quit();
+        app.quit()
       }
-    });
+    })
   } else {
     process.on('SIGTERM', () => {
-      app.quit();
-    });
+      app.quit()
+    })
   }
 }
 
 app.on('second-instance', () => {
   if (win) {
-    if (win.isMinimized()) win.restore();
-    win.focus();
+    if (win.isMinimized()) win.restore()
+    win.focus()
   }
-});
+})
 
 const menuTemplate: any = [
   ...(isMac
@@ -137,7 +181,7 @@ const menuTemplate: any = [
           {
             label: '退出',
             click: () => {
-              app.quit();
+              app.quit()
             }
           }
         ]
@@ -173,9 +217,7 @@ const menuTemplate: any = [
       {
         label: '帮助',
         click: async () => {
-          await shell.openExternal(
-            'https://github.com/funnyzak/tts-now#readme'
-          );
+          await shell.openExternal('https://github.com/funnyzak/tts-now#readme')
         }
       },
       {
@@ -183,36 +225,14 @@ const menuTemplate: any = [
         click: async () => {
           await shell.openExternal(
             'https://github.com/funnyzak/tts-now/releases/latest'
-          );
+          )
         }
       }
     ]
   }
-];
-const menu = Menu.buildFromTemplate(menuTemplate);
+]
+const menu = Menu.buildFromTemplate(menuTemplate)
 
 if (!isDevelopment) {
-  Menu.setApplicationMenu(menu);
+  Menu.setApplicationMenu(menu)
 }
-
-// 选择要转换的文件或其他选择
-ipcMain.on('select_files', (event, arg) => {
-  console.log(event, arg);
-
-  // https://www.electronjs.org/docs/latest/api/dialog
-  dialog
-    .showOpenDialog(win, {
-      title: '选择文件',
-      buttonLabel: '确定',
-      properties: ['openFile', 'multiSelections'],
-      filters: [{ name: '文件', extensions: ['txt', 'text'] }],
-      ...(arg.config || {})
-    })
-    .then((_rlt) => {
-      console.log(_rlt);
-      event.reply('selected_files', {
-        data: _rlt,
-        action: arg.action
-      });
-    });
-});
